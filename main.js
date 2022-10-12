5cb49107d6ebe9856ca250aaeb8f46bb5340c7ae
chainsawsteel1 <114403771+chainsawsteel1@users.noreply.github.com>
2022-10-12 17:23:43 +0900

    Update main.js
    大幅

--- main.js
+++ main.js
@@ -1,82 +1,82 @@
-particlesJS("particles-js", {
-  particles: {
-    number: {
-      value: 1250, //この数値を変更すると紙吹雪の数が増減できる
-      density: {
-        enable: false,
-        value_area: 400
-      }
-    },
-    color: {
-      value: [
-        "#EA5532",
-        "#F6AD3C",
-        "#FFF33F",
-        "#00A95F",
-        "#00ADA9",
-        "#00AFEC",
-        "#4D4398",
-        "#E85298"
-      ] //紙吹雪の色の数を増やすことが出来る
-    },
-    shape: {
-      type: "polygon", //形状はpolygonを指定
-      stroke: {
-        width: 0
-      },
-      polygon: {
-        nb_sides: 5 //多角形の角の数
-      }
-    },
-    opacity: {
-      value: 1,
-      random: false,
-      anim: {
-        enable: true,
-        speed: 20,
-        opacity_min: 0,
-        sync: false
-      }
-    },
-    size: {
-      value: 5.305992965476349,
-      random: true, //サイズをランダムに
-      anim: {
-        enable: true,
-        speed: 1.345709068776642,
-        size_min: 0.8,
-        sync: false
-      }
-    },
-    line_linked: {
-      enable: false
-    },
-    move: {
-      enable: true,
-      speed: 10, //この数値を小さくするとゆっくりな動きになる
-      direction: "bottom", //下に向かって落ちる
-      random: false, //動きはランダムにならないように
-      straight: false, //動きをとどめない
-      out_mode: "out", //画面の外に出るように描写
-      bounce: false, //跳ね返りなし
-      attract: {
-        enable: false,
-        rotateX: 600,
-        rotateY: 1200
-      }
-    }
-  },
-  interactivity: {
-    detect_on: "canvas",
-    events: {
-      onhover: {
-        enable: false
-      },
-      onclick: {
-        enable: false
-      },
-      resize: true
-    }
-  },
-  retina_detect: true
+particlesJS("particles-js",{
+	"particles":{
+		"number":{
+			"value":38,//この数値を変更すると幾何学模様の数が増減できる
+			"density":{
+				"enable":true,
+				"value_area":800
+			}
+		},
+		"color":{
+			"value":"#ffffff"//色
+		},
+		"shape":{
+			"type":"polygon",//形状はpolygonを指定
+			"stroke":{
+				"width":0,
+			},
+	"polygon":{
+		"nb_sides":3//多角形の角の数
+	},
+	"image":{
+		"width":190,
+		"height":100
+	}
+	},
+		"opacity":{
+		"value":0.664994832269074,
+		"random":false,
+		"anim":{
+			"enable":true,
+			"speed":2.2722661797524872,
+			"opacity_min":0.08115236356258881,
+			"sync":false
+		}
+		},
+		"size":{
+			"value":3,
+			"random":true,
+			"anim":{
+				"enable":false,
+				"speed":40,
+				"size_min":0.1,
+				"sync":false
+			}
+		},
+		"line_linked":{
+			"enable":true,
+			"distance":150,
+			"color":"#ffffff",
+			"opacity":0.6,
+			"width":1
+		},
+		"move":{
+			"enable":true,
+			"speed":6,//この数値を小さくするとゆっくりな動きになる
+			"direction":"none",//方向指定なし
+			"random":false,//動きはランダムにしない
+			"straight":false,//動きをとどめない
+			"out_mode":"out",//画面の外に出るように描写
+			"bounce":false,//跳ね返りなし
+			"attract":{
+				"enable":false,
+				"rotateX":600,
+				"rotateY":961.4383117143238
+			}
+		}
+	},
+	"interactivity":{
+		"detect_on":"canvas",
+		"events":{
+			"onhover":{
+				"enable":false,
+				"mode":"repulse"
+			},
+	"onclick":{
+		"enable":false
+	},
+	"resize":true
+		}
+	},
+	"retina_detect":true
 });
