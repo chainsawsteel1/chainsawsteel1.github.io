c1dac1e571a5f841a1cc36248983e6f9a8b8d87b
chainsawsteel1 <114403771+chainsawsteel1@users.noreply.github.com>
2022-10-13 14:49:53 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,3 +2,10 @@ $(window).on('load',function(){
   $("#splash").delay(1500).fadeOut('slow');//ローディング画面を1.5秒（1500ms）待機してからフェードアウト
   $("#splash_logo").delay(1200).fadeOut('slow');//ロゴを1.2秒（1200ms）待機してからフェードアウト
 });
+
+
+$("#particle").particleText({
+text: "‾\_ツ_/‾", // 表示させたいテキスト。改行の場合は<br>追加
+colors:["#fff","#ccc", "#ddd"], // パーティクルの色を複数指定可能
+speed: "high", // slow, middle, high の3つから粒子が集まる速さを選択
+});
