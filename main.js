45e9aa5c2812389e109db3f4a52cba0ae2f56181
chainsawsteel1 <114403771+chainsawsteel1@users.noreply.github.com>
2022-10-13 14:59:13 +0900

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ $(window).on('load',function(){
 
 
 $("#particle").particleText({
-text: "‾\_ツ_/‾", // 表示させたいテキスト。改行の場合は<br>追加
+text: "🤷", // 表示させたいテキスト。改行の場合は<br>追加
 colors:["#fff","#ccc", "#ddd"], // パーティクルの色を複数指定可能
 speed: "high", // slow, middle, high の3つから粒子が集まる速さを選択
 });
