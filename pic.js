2a5d6c5880bf768ab599b34cfe57b1baad77587d
chainsawsteel1 <114403771+chainsawsteel1@users.noreply.github.com>
2022-10-14 14:20:16 +0900

    tyottoteuto

--- pic.js
+++ pic.js
@@ -1,5 +1,5 @@
 $(window).scroll(function() {
-        var scroll = $(window).scrollTop();//スクロール値を定義
+       // var scroll = $(window).scrollTop();//スクロール値を定義
     //header-imgの背景
 	$('#header-img').css({
 			transform: 'scale('+(100 + scroll/10)/100+')',//スクロール値を代入してscale1から拡大.scroll/10の値を小さくすると拡大値が大きくなる
