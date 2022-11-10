abbe58eaba28c6eb0b56ae1a36818b970cfe4fe1
chainsawsteel1 <114403771+chainsawsteel1@users.noreply.github.com>
2022-11-10 22:37:23 +0900

    Delete pic.js

--- pic.js
+++
@@ -1,8 +0,0 @@
-$(window).scroll(function() {
-       var scroll = $(window).scrollTop();//スクロール値を定義
-//header-imgの背景
-	$('#header-img').css({
-			transform: 'scale('+(100 + scroll/10)/100+')',//スクロール値を代入してscale1から拡大.scroll/10の値を小さくすると拡大値が大きくなる
-			top: -(scroll/50)  + "%",//スクロール値を代入してtopの位置をマイナスにずらす
-        });
-    });
