a3d8ddfbd1af446212599f2b97d5447eda316656
chainsawsteel1 <shimasoushun@gmail.com>
2023-11-05 10:46:13 +0900

    ブログページ仮実装

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//画面サイズ
 $(window).on('load resize', function(){
 var winW = $(window).width();
 var devW = 650;
@@ -7,29 +8,50 @@ if (winW <= devW) {
     $(".warn").css("animation-name","none");
 }
 });
-
+//ロード表示
 $(window).on("load", function () {
     $(".load").css("animation-name","load");
     $('.loadmark').addClass('okload');
     $('.font').addClass('okload-b');
 });
-
+//相互リンク置き場
 $(function() {
     $(".iboxo").click(function() {
         $(".ibox").slideToggle();
     });
 });
-
+//通知押したときの
 $(function() {
     $(".warn").click(function() {
         $('.warnb').removeClass('viewbu');
         $('.warnb').addClass('viewb');
     });
 });
-
+//ポップアップ的なの消去
 $(function() {
     $(".warnb").click(function() {
         $('.warnb').addClass('viewbu');
         $('.warnb').removeClass('viewb');
+    });
+});
+
+
+
+//ブログボタン
+$(function() {
+    $(".btnblog").click(function() {
+        $('.maincnt').removeClass('viewcnt');
+        $('.blog').addClass('viewcnt');
+        $('.maincnt').addClass('hidecnt');
+        $('.blog').removeClass('hidecnt');
+    });
+});
+//戻るボタン
+$(function() {
+    $(".btnmain").click(function() {
+        $('.maincnt').addClass('viewcnt');
+        $('.blog').removeClass('viewcnt');
+        $('.maincnt').removeClass('hidecnt');
+        $('.blog').addClass('hidecnt');
     });
 });
