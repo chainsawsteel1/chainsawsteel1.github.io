ef7007a028496a002c272bb71f98f647a92801de
chainsawsteel1 <shimasoushun@gmail.com>
2023-11-06 20:48:05 +0900

    ダイナミックカラー辞めた

--- main.js
+++ main.js
@@ -1,35 +1,35 @@
 //画面サイズ
-$(window).on('load resize', function(){
-var winW = $(window).width();
-var devW = 650;
-if (winW <= devW) {
-    $(".warn").css("animation-name","warn");
-} else {
-    $(".warn").css("animation-name","none");
-}
+$(window).on('load resize', function () {
+    var winW = $(window).width();
+    var devW = 650;
+    if (winW <= devW) {
+        $(".warn").css("animation-name", "warn");
+    } else {
+        $(".warn").css("animation-name", "none");
+    }
 });
 //ロード表示
 $(window).on("load", function () {
-    $(".load").css("animation-name","load");
+    $(".load").css("animation-name", "load");
     $('.loadmark').addClass('okload');
     $('.font').addClass('okload-b');
 });
 //相互リンク置き場
-$(function() {
-    $(".iboxo").click(function() {
+$(function () {
+    $(".iboxo").click(function () {
         $(".ibox").slideToggle();
     });
 });
 //通知押したときの
-$(function() {
-    $(".warn").click(function() {
+$(function () {
+    $(".warn").click(function () {
         $('.warnb').removeClass('viewbu');
         $('.warnb').addClass('viewb');
     });
 });
 //ポップアップ的なの消去
-$(function() {
-    $(".warnb").click(function() {
+$(function () {
+    $(".warnb").click(function () {
         $('.warnb').addClass('viewbu');
         $('.warnb').removeClass('viewb');
     });
@@ -38,8 +38,8 @@ $(function() {
 
 
 //ブログボタン
-$(function() {
-    $(".btnblog").click(function() {
+$(function () {
+    $(".btnblog").click(function () {
         $('.maincnt').removeClass('viewcnt');
         $('.blog').addClass('viewcnt');
         $('.maincnt').addClass('hidecnt');
@@ -47,8 +47,8 @@ $(function() {
     });
 });
 //戻るボタン
-$(function() {
-    $(".btnmain").click(function() {
+$(function () {
+    $(".btnmain").click(function () {
         $('.maincnt').addClass('viewcnt');
         $('.blog').removeClass('viewcnt');
         $('.maincnt').removeClass('hidecnt');
