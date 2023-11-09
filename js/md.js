09d22b4efa1dc2c62e0bde2410ce21645781a4a2
chainsawsteel1 <shimasoushun@gmail.com>
2023-11-09 19:01:36 +0900

    irir

--- js/md.js
+++ js/md.js
@@ -8,12 +8,12 @@ const loadMarkdown = async () => {
     converter.setFlavor('github');
 
     // ?記号以降の文字列を取得
-    let filename = "md/" + (window.location.search.substring(1));
+    let filename = "/md/" + (window.location.search.substring(1) + ".md");
 
     let text = await (await fetch(filename)).text();
     console.log(text);
 
-    if (text.match(new RegExp("<pre>Cannot GET .*\.md</pre>"))) {
+    if (text.match(new RegExp("<pre>Cannot GET .*/.md</pre>"))) {
       throw new Error("ファイル見つからん");
     }
 
