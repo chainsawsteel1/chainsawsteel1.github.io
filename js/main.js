8886f5ff81597ed734145a6cb7506186be1e43cc
chainsawsteel1 <114403771+chainsawsteel1@users.noreply.github.com>
2024-02-17 17:19:28 +0900

    anime-syonn

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 //画面サイズ
 $(window).on('load resize', function () {
-    var winW = $(window).width();
-    var devW = 650;
-    if (winW <= devW) {
-        $("body").css("width", "100%");
-    } else {
-        $("body").css("width", "60%");
-    }
+  var winW = $(window).width();
+  var devW = 650;
+  if (winW <= devW) {
+    $("body").css("width", "100%");
+  } else {
+    $("body").css("width", "60%");
+  }
 });
 
 //navbarの透過
@@ -18,4 +18,46 @@ function opacity_nav() {
   } else if (window.scrollY < heig) {
     $('.navbar').removeClass('shadow');
   }
+}
+
+//ロード表示
+$(window).on("load", function () {
+  setTimeout(function () {
+    $('.content').removeClass('load');
+  }, 200);
+});
+
+//遅延
+$(document).on('click', 'a', function (e) {
+  e.preventDefault();
+
+  const blank = $(this).attr('target');
+  const fromDomain = location.origin;
+  const from = fromDomain + location.pathname + location.search;
+  const to = $(this).prop('href');
+  const check = to.replace(from, '');
+
+  if (blank == '_blank' || to.indexOf(fromDomain) == -1) {
+    gooutpage(to);
+  } else if (check.indexOf('#') == 0 || check == '') {
+    var target = 0;
+    if (check != '') {
+      target = $(check).offset().top;
+    }
+    gotop(target);
+  } else {
+    inpagego(to);
+  }
+});
+function gooutpage(target) {
+  window.open(target, '_blank');
+}
+function inpagego(target) {
+  $('.content').addClass('next');
+  setTimeout(function () {
+    window.location = target;
+  }, 300);
+}
+function gotop(target) {
+  $('body,html').stop().animate({ scrollTop: target }, 300);
 }
