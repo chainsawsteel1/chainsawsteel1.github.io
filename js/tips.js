bd7cd94406e9bfbb1394f57045f590483c3d1461
chainsawsteel1 <114403771+chainsawsteel1@users.noreply.github.com>
2024-05-19 19:04:37 +0900

    add tips

---
+++ js/tips.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+        //対象の配列
+        const subjectArray = [
+            "Firefoxを推奨したいが、chromium系のが快適という事実",
+            "戻る操作をするとLoadingで固まることがある",
+            "Starter Quests Of Create",
+            "SEOのこと考えてたらこんな構造にはしないよね普通"
+        ];
+        //ランダムに1つ取得
+        const subject = subjectArray[Math.floor(Math.random() * subjectArray.length)];
+        //書き出す
+        document.getElementById("tips").innerText = subject;
+});
