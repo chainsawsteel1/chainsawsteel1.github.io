cbbf58d27488d8be4bf2b94104fac5c25977de9d
chainsawsteel1 <114403771+chainsawsteel1@users.noreply.github.com>
2024-05-19 19:08:15 +0900

    at

--- js/tips.js
+++ js/tips.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
             "Firefoxを推奨したいが、chromium系のが快適という事実",
             "戻る操作をするとLoadingで固まることがある",
             "Starter Quests Of Create",
+            "余計な待ち時間をあなたへ",
             "SEOのこと考えてたらこんな構造にはしないよね普通"
         ];
         //ランダムに1つ取得
