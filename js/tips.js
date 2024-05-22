f57e2895e447ed9b580853474e766cc97f2dbd77
chainsawsteel1 <114403771+chainsawsteel1@users.noreply.github.com>
2024-05-22 23:30:24 +0900

    Update tips.js

--- js/tips.js
+++ js/tips.js
@@ -9,7 +9,6 @@ function tips(id) {
         "戻る操作をするとLoadingで固まることがある",
         "余計な待ち時間をあなたへ",
         "<img src=\"/img/sqoc.png\" alt=\"sqoc\" width=\"100px\">",
-        "自分の画像の著作権全部AllRightsReservedでよくね"
     ];
     //ランダムに1つ取得
     const subject = subjectArray[Math.floor(Math.random() * subjectArray.length)];
